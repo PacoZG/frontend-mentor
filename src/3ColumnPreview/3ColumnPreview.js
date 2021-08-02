45c0b45476766cb04b11bb42508bd8160dc369d1
Francisco Zavala <pako.zavala@gmail.com>
2021-08-02 22:58:54 +0300

    fixed

--- src/3ColumnPreview/3ColumnPreview.js
+++ src/3ColumnPreview/3ColumnPreview.js
@@ -6,41 +6,39 @@ import luxury from "./images/icon-luxury.svg";
 
 const TCP = () => {
   return (
-    <div className="container">
-      <div className="main">
-        <div className="card1">
-          <img className="img" src={suvs} alt="suvs" />
-          <h1 className="header">SEDANS</h1>
-          <p className="paraph">
-            Choose a sedan for its affordability and excellent fuel economy.
-            Ideal for cruising in the city or on your next road trip.
-          </p>
-          <label className="label1" onClick="">
-            Learn More
-          </label>
-        </div>
-        <div className="card2">
-          <img className="img" src={sedans} alt="sedans" />
-          <h1 className="header">SUVS</h1>
-          <p className="paraph">
-            Choose a sedan for its affordability and excellent fuel economy.
-            Ideal for cruising in the city or on your next road trip.
-          </p>
-          <label className="label2" onClick="">
-            Learn More
-          </label>
-        </div>
-        <div className="card3">
-          <img className="img" src={luxury} alt="luxury" />
-          <h1 className="header">LUXURY</h1>
-          <p className="paraph">
-            Choose a sedan for its affordability and excellent fuel economy.
-            Ideal for cruising in the city or on your next road trip.
-          </p>
-          <label className="label3" onClick="">
-            Learn More
-          </label>
-        </div>
+    <div className="main">
+      <div className="card1">
+        <img className="img" src={suvs} alt="suvs" />
+        <h1 className="header">SEDANS</h1>
+        <p className="paraph">
+          Choose a sedan for its affordability and excellent fuel economy. Ideal
+          for cruising in the city or on your next road trip.
+        </p>
+        <label className="label1" onClick="">
+          Learn More
+        </label>
+      </div>
+      <div className="card2">
+        <img className="img" src={sedans} alt="sedans" />
+        <h1 className="header">SUVS</h1>
+        <p className="paraph">
+          Choose a sedan for its affordability and excellent fuel economy. Ideal
+          for cruising in the city or on your next road trip.
+        </p>
+        <label className="label2" onClick="">
+          Learn More
+        </label>
+      </div>
+      <div className="card3">
+        <img className="img" src={luxury} alt="luxury" />
+        <h1 className="header">LUXURY</h1>
+        <p className="paraph">
+          Choose a sedan for its affordability and excellent fuel economy. Ideal
+          for cruising in the city or on your next road trip.
+        </p>
+        <label className="label3" onClick="">
+          Learn More
+        </label>
       </div>
     </div>
   );
